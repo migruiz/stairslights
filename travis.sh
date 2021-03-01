f6813f2c1c42d6feb07e7f0d730777f3e7c997c3
Miguel <mig.ruiz@gmail.com>
2021-03-01 13:35:24 +0000

    using new hub repo

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 set -ev
-HUBNAME=migruiz/entrance-lights;
+HUBNAME=migruiz/stairslights;
 docker pull $HUBNAME || true
 docker build  --cache-from $HUBNAME  -t $HUBNAME  -f rpi/Dockerfile .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
