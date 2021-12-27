48c908500d9b963661238e3d21e1a77b8e022fff
Miguelr <mig.ruiz@gmail.com>
2021-12-27 23:27:04 +0000

    testing wheel

--- App/app.js
+++ App/app.js
@@ -1,9 +1,9 @@
-const { Observable,merge,timer } = require('rxjs');
-const { mergeMap, map,share,filter,mapTo,take,debounceTime,throttle,throttleTime} = require('rxjs/operators');
+const { Observable,merge,timer, interval } = require('rxjs');
+const { mergeMap, map,share,filter,mapTo,take,debounceTime,throttle,throttleTime, startWith, takeWhile, delay, scan, distinct} = require('rxjs/operators');
 var mqtt = require('./mqttCluster.js');
 
-global.mtqqLocalPath = process.env.MQTTLOCAL;
-//global.mtqqLocalPath = 'mqtt://piscos.tk';
+//global.mtqqLocalPath = process.env.MQTTLOCAL;
+global.mtqqLocalPath = 'mqtt://192.168.0.11';
 
 
 const GROUND_FLOOR_SENSOR_TOPIC = process.env.GROUND_FLOOR_SENSOR_TOPIC
@@ -20,6 +20,44 @@ const DAYBRIGHTNESS = process.env.DAYBRIGHTNESS
 
 
 console.log(`starting stairs lights current time ${new Date()}`)
+
+const source = interval(20).pipe(
+    startWith(1),
+    scan((acc, curr) => {
+        if (acc.value > 500) return {value: 500, direction:'down'}
+        else if (acc.value <= 0) return {value: 1, direction:'up'}
+        else   return {value: acc.value + 10 * ( acc.direction==='up'? 1 : -1 ), direction:acc.direction}
+    }, {value:0, direction:'up'}),
+    
+    //delay(4000)
+    )
+
+
+
+ const rotationCoreSensor = new Observable(async subscriber => {  
+    var mqttCluster=await mqtt.getClusterAsync()   
+    mqttCluster.subscribeData('zigbee2mqtt/0x0c4314fffeb064fb', function(content){        
+            subscriber.next({content})
+    });
+});
+
+const sharedRotatiobStream = rotationCoreSensor.pipe(share())
+const rotationSensorStream = sharedRotatiobStream.pipe(
+    filter( m => m.content.action==='rotate_right' ||  m.content.action==='rotate_left' || m.content.action==='rotate_stop'),
+    map( m => ({action: m.content.action})),
+    scan((acc, curr) => ((acc.action==curr.action)? acc: curr)
+    , {action:'idle'}),
+    distinct()
+)
+
+
+rotationSensorStream.subscribe(async val => {
+    console.log(val);
+    //(await mqtt.getClusterAsync()).publishMessage('stairs/down/light',`${val.value}`);
+ });
+
+
+return;
 
 const groundfloorSensorStream = new Observable(async subscriber => {  
     var mqttCluster=await mqtt.getClusterAsync()   
@@ -78,4 +116,5 @@ merge(upstairsLightsOnStream,upstairsLightsOffStream)
     console.log('Upstairs', m);
     (await mqtt.getClusterAsync()).publishMessage('stairs/up/light',m)
 })
+
 
