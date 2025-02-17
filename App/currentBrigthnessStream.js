ec2496bea6663e0ea48961acaa0f5cca3c4fb2bc
Sonia Blanco <soniab@tds.ie>
2025-02-17 17:53:54 +0000

    fixing ikea rotation

--- App/currentBrigthnessStream.js
+++ App/currentBrigthnessStream.js
@@ -11,7 +11,7 @@ const upstairsRotationDeviceStream = getRawRotationDeviceStream('zigbee2mqtt/0x0
 const downstairsTuyaRotationDeviceStream = getRawTuyaRotationDeviceStream('zigbee2mqtt/0xa4c138712f6a2d0b')
 
 
-const increase = (acc)=>{
+const increaseTuya = (acc)=>{
     if (acc < 10){
         return  acc + 4;
     }
@@ -19,7 +19,7 @@ const increase = (acc)=>{
         return acc + 200 > 1000 ? 1000 : acc + 200;
     }
 }
-const decrease = (acc)=>{
+const decreaseTuya = (acc)=>{
     if (acc < 10){
         return acc - 4 < 1 ? 1 : acc - 4;
     }
@@ -30,16 +30,37 @@ const decrease = (acc)=>{
 
 
 
+const increaseIkea = (acc)=>{
+    if (acc < 20){
+        return  acc + 1;
+    }
+    else {
+        return acc + 40 > 1000 ? 1000 : acc + 40;
+    }
+}
+const decreaseIkea = (acc)=>{
+    if (acc < 20){
+        return acc - 1 < 1 ? 1 : acc - 1 ;
+    }
+    else {
+        return acc - 40 < 20 ? 20 - 1 : acc - 40;
+    }
+}
+
+
+
+
+
 
 const currentBrigthnessStream = merge(downstairsRotationDeviceStream,upstairsRotationDeviceStream,dayTimeStream,downstairsTuyaRotationDeviceStream, of({action:'init'}).pipe(delay(2000))).pipe(
     scan((acc, curr) => {
         if (curr.action==='date_time') return {triggeredBy:'timeOfDay', value:curr.value}
         if (curr.action==='toggle') return {triggeredBy:'rotationDevice', value:(acc.value==0 ? getDefaultBrightness() : 0)}
         if (curr.action==='single') return {triggeredBy:'rotationDevice', value:(acc.value==0 ? getDefaultBrightness() : 0)}
-        if (curr.action==='brightness_move_up' && curr.type==='ikea') return {triggeredBy:'rotationDevice', value: increase(acc.value) }
-        if (curr.action==='brightness_move_down' && curr.type==='ikea') return {triggeredBy:'rotationDevice', value: decrease(acc.value) }
-        if (curr.action==='rotate_right' && curr.type==='tuya') return {triggeredBy:'rotationDevice', value: increase(acc.value) }
-        if (curr.action==='rotate_left' && curr.type==='tuya') return {triggeredBy:'rotationDevice', value: decrease(acc.value) }
+        if (curr.action==='brightness_move_up' && curr.type==='ikea') return {triggeredBy:'rotationDevice', value: increaseIkea(acc.value) }
+        if (curr.action==='brightness_move_down' && curr.type==='ikea') return {triggeredBy:'rotationDevice', value: decreaseIkea(acc.value) }
+        if (curr.action==='rotate_right' && curr.type==='tuya') return {triggeredBy:'rotationDevice', value: increaseTuya(acc.value) }
+        if (curr.action==='rotate_left' && curr.type==='tuya') return {triggeredBy:'rotationDevice', value: decreaseTuya(acc.value) }
         if (curr.action==='init') return {triggeredBy:'init', value: 100 }
         
     }, {triggeredBy:'init', value:50}),    
